d7ad57cd250830d30d83eb341d45f3d86d06d810
Shah Alam <shahalam01660166344@gmail.com>
2023-11-30 10:26:17 +0600

    added bookings and user related code

--- src/Pages/HomePage/Dashboard/DeliveryMan/MyDelivery.jsx
+++ src/Pages/HomePage/Dashboard/DeliveryMan/MyDelivery.jsx
@@ -1,25 +1,40 @@
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 import SectionTitle from "../../../../Components/SectionTitle/SectionTitle";
+import useBookings from "../../../../hooks/useBooking";
+import { useQuery } from "@tanstack/react-query";
+
 
 const MyDelivery = () => {
-    const axiosSecure = useAxiosSecure();
 
-    const { data: users = [], refetch } = useQuery({
-        queryKey: ["users"],
-        queryFn: async () => {
-          const res = await axiosSecure.get("/users");
+  const { bookings, refetch } = useBookings();
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      const filteredUsers = res.data.filter(
+        (user) => user.role === "deliveryMan"
+      );
 
-          // Filter users based on role (modify as needed)
-          const filteredUsers = res.data.filter(
-            (user) => user.role === "deliveryMan"
-          );
+      return filteredUsers;
+    },
+  });
 
-          return filteredUsers;
-        },
-      });
+  const deliveryMan = users.length > 0 ? users[0] : null;
 
-  const handleCancelParcel = () => {
+  // Filter bookings based on the deliveryMan's _id
+  const forDeliveryMan = bookings.filter(
+    (booking) =>
+      booking.deliveryManId === (deliveryMan ? deliveryMan._id : null)
+  );
+
+  // Display the filtered bookings
+  console.log(forDeliveryMan,bookings);
+
+  // console.log(user.email, bookings,users)
+
+  const handleDeleteItem = () => {
     Swal.fire({
       title: "Are you sure?",
       text: "You won't be able to revert this!",
@@ -27,10 +42,18 @@ const MyDelivery = () => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, cancel it!",
-    }).then((result) => {
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        // Trigger the cancel parcel mutation
+        const res = await axiosSecure.delete(`/bookings/${forDeliveryMan._id}`);
+        if (res.data.deletedCount > 0) {
+          refetch();
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your Parcel has been deleted.",
+            icon: "success",
+          });
+        }
       }
     });
   };
@@ -52,70 +75,73 @@ const MyDelivery = () => {
   };
 
   return (
-    <> <div>
-   <SectionTitle heading={"My delivery"}></SectionTitle>
-      <h1 className="text-3xl font-bold mb-4">
-     {/* My Delivery List: {parcels.length} */}
-      </h1>
-   <div className="overflow-x-auto rounded-xl">
-     <table className="table">
-       <thead className="bg-[#26DEBE] uppercase text-gray-600">
-         <tr>
-              <th>User Name</th>
-              <th>Receiver Name</th>
-           <th>User Phone</th>
-              <th>Requested Date</th>
-              <th>Approximate Date</th>
-           <th>Receiver Number</th>
-           <th>Receiver Address</th>
-           <th>Location</th>
-           <th>Cancel</th>
-           <th>Deliver</th>
-         </tr>
-       </thead>
-       <tbody>
-         {"parcels"?.map((parcel) => (
-              <tr key={parcel._id}>
-                <td>{parcel.name}</td>
-                <td>{parcel.receiverName}</td>
-                <td>{parcel.phoneNumber}</td>
-                <td>{parcel.bookingDate}</td>
-                <td>{parcel.requestedDeliveryDate}</td>
-                <td>{parcel.receiverPhoneNumber}</td>
-                <td>{parcel.parcelDeliveryAddress}</td>
-                <td>
-                  {/* Add a button or link to view location */}
-                  <button
-                    className="btn btn-outline btn-primary"
-                    onClick={() => {
-                      // Handle view location
-                    }}
-                  >
-                    Location
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-outline btn-danger"
-                    onClick={() => handleCancelParcel(parcel._id)}
-                  >
-                    Cancel
-                  </button>
-                </td>
-                <td>
-                  <button
-                    className="btn btn-outline btn-success"
-                    onClick={() => handleDeliverParcel(parcel._id)}
-                  >
-                    Deliver
-                  </button>
-                </td>
+    <>
+      {" "}
+      <div>
+        <SectionTitle heading={"My delivery"}></SectionTitle>
+        <h1 className="text-3xl font-bold mb-4">
+          {/* My Delivery List: {parcels.length} */}
+        </h1>
+        <div className="overflow-x-auto rounded-xl">
+          <table className="table">
+            <thead className="bg-[#26DEBE] uppercase text-gray-600">
+              <tr>
+                <th>User Name</th>
+                <th>Receiver Name</th>
+                <th>User Phone</th>
+                <th>Requested Date</th>
+                <th>Approximate Date</th>
+                <th>Receiver Number</th>
+                <th>Receiver Address</th>
+                <th>Location</th>
+                <th>Cancel</th>
+                <th>Deliver</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {forDeliveryMan?.map((parcel) => (
+                <tr key={parcel._id}>
+                  <td>{parcel.name}</td>
+                  <td>{parcel.receiverName}</td>
+                  <td>{parcel.phoneNumber}</td>
+                  <td>{parcel.bookingDate}</td>
+                  <td>{parcel.requestedDeliveryDate}</td>
+                  <td>{parcel.receiverPhoneNumber}</td>
+                  <td>{parcel.parcelDeliveryAddress}</td>
+                  <td>
+                    {/* Add a button or link to view location */}
+                    <button
+                      className="btn btn-outline btn-primary"
+                      onClick={() => {
+                        // Handle view location
+                      }}
+                    >
+                      Location
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-outline btn-danger"
+                      onClick={() => handleDeleteItem(parcel._id)}
+                    >
+                      Cancel
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-outline btn-success"
+                      onClick={() => handleDeliverParcel(parcel._id)}
+                    >
+                      Deliver
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div></>
+    </>
   );
 };
 
